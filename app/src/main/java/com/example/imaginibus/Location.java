561e6a6bf4ca04f604bc446217596975bba3da0a
MinhVu2018 <tvuminht@gmail.com>
2021-03-02 23:04:20 +0700

    create custom dialog and add some onClickListeners

--- app/src/main/java/com/example/imaginibus/Location.java
+++ app/src/main/java/com/example/imaginibus/Location.java
@@ -2,13 +2,36 @@ package com.example.imaginibus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Location extends AppCompatActivity {
-
+    ImageButton btn_back, btn_option;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        SetUpButton();
+    }
+
+    private void SetUpButton() {
+        btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Location.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_option = (ImageButton) findViewById(R.id.btn_option);
+        btn_option.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Location.this, MainActivity.class);    // not yet
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/imaginibus/MainActivity.java
+++ app/src/main/java/com/example/imaginibus/MainActivity.java
@@ -6,21 +6,75 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btn_search, btn_option, btn_gallery, btn_video, btn_location, btn_favorite, btn_secure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        SetUpButton();
+    }
+
+    private void SetUpButton(){
+        btn_search = (ImageButton) findViewById(R.id.btn_search);
+        btn_search.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, ByDate.class);
-                MainActivity.this.startActivity(myIntent);
+                PreviewImage dialog = new PreviewImage();
+                dialog.show(getSupportFragmentManager(), "show preview image");
             }
         });
+
+        btn_option = (ImageButton) findViewById(R.id.btn_option);
+        btn_option.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                PreviewImage dialog = new PreviewImage();
+                dialog.show(getSupportFragmentManager(), "show preview image");
+            }
+        });
+
+        btn_gallery = (ImageButton) findViewById(R.id.btn_gallery);
+        btn_gallery.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ByDate.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_video = (ImageButton) findViewById(R.id.btn_video);
+        btn_video.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ByDate.class);    // not yet
+                startActivity(intent);
+            }
+        });
+
+        btn_location = (ImageButton) findViewById(R.id.btn_location);
+        btn_location.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Location.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_favorite = (ImageButton) findViewById(R.id.btn_favorite);
+        btn_favorite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Favorite.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_secure = (ImageButton) findViewById(R.id.btn_secure);
+        btn_secure.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ByDate.class);    // not yet
+                startActivity(intent);
+            }
+        });
+
     }
 }
