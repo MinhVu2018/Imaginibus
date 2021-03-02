bfae337896f7a67d98e6f2faed631fb82fc96e1c
MinhVu2018 <tvuminht@gmail.com>
2021-03-02 14:47:28 +0700

    3 screen (ByDate, Favorite, Location) and some images

--- app/src/main/java/com/example/imaginibus/MainActivity.java
+++ app/src/main/java/com/example/imaginibus/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.imaginibus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, ByDate.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
