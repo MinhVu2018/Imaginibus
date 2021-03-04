f729bb1d646ab9fdbd2739a9072364fefaeeb2b9
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2021-03-04 13:06:48 +0700

    Add secure register and secure album layout

---
+++ app/src/main/java/com/example/imaginibus/SecureRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.imaginibus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecureRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secure_register);
+    }
+}
